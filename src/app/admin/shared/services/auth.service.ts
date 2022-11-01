2e4017a0f32dc738361300d6e1ea4152d1de65cf
Viktor Lachkov <EPetrov2@gmail.com>
2022-11-01 16:39:40 +0400

    Setub firebase auth

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { User } from "src/app/shared/interfaces";
+import { FbAuthResponse, User } from "src/app/shared/interfaces";
+import { environment } from "src/environments/environment";
+import { tap } from "rxjs/operators";
+
 
 @Injectable()
 
@@ -13,7 +16,11 @@ export class AuthService {
     }
 
     login( user: User): Observable<any> {
-        return this.http.post('', user)
+        return this.http.post(
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+            .pipe(
+                tap(this.setToken as Object)
+            )
     }
     logout() {
 
@@ -23,7 +30,8 @@ export class AuthService {
         return !!this.token;
     }
 
-    private setToken() {
-
+    private setToken(response: FbAuthResponse) {
+        console.log(response.idToken);
+        
     }
 }
